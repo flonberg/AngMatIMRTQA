4b67356061afce7939d785a3e6a120c592847d89
Franklin Lonberg <flonberg@gmail.com>
2021-09-03 16:18:55 -0400

    displaying plans array in accordion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,61 +34,7 @@ export class AppComponent {
     //  this.setData(res);
    // })
    }
-   setDate(start, end){
-     console.log("eve is %o", start)
-   }
-   /**
-    *  get the plans for a single MD, ION-endPt looks for 'test' to make it return MD list 
-    */
- getMDs(){
-  var url = 'https://whiteboard.partners.org/esb/FLwbe/proxy.php?MDKey=test';
-  return this .http.get(url)
-  }
- setData(res ) {
-   console.log("111")
-   this.data = res;
-   console.log(this.data)
- }
- getPlanData(n){
-   this .MDName = n;
-   this .MDKeySelected = this .data[n];
-   console.log("n is %o  key is %o", n, this.data[n]);
-   if (this .dateRangeStart)
-    var url = 'https://whiteboard.partners.org/esb/FLwbe/proxy.php?MDKey=' + this.data[n] + '&startDate=' + this .dateRangeStart +
-                                                                                          '&endDate=' + this .dateRangeEnd;
-        console.log('url 57 is %o', url)
-   this .http.get(url).subscribe(res =>{
-     this. setPlanData(res)
-   })
- }
+ 
 
- dateRangeChange(dateRangeStart: HTMLInputElement, dateRangeEnd: HTMLInputElement) {
-   var tDate = new Date(dateRangeStart.value)
-    this .dateRangeStart = this.datePipe.transform(tDate, 'yyyy-MM-dd')
-   if (  dateRangeEnd.value  ){
-    var eDate = new Date(dateRangeEnd.value)
-   this .dateRangeEnd = this.datePipe.transform(eDate, 'yyyy-MM-dd')
- }
-
-}
- setPlanData(res){
-  let areas = new Array<Array<any>>();
-  this.planData = res;
-  console.log("planData is %o ", this.planData)
-  let tStr = this .MDName + "," + this .dateRangeStart + "," + this.dateRangeEnd + "\r\n";      // put in MdName, StartDate and EndDate
-  tStr += "\r\n Days, 1,2,3,4,5,6,7,8,9,10,11,12,>12,\r\n";
-    Object.keys(res[0]).forEach(key2 => {
-      console.log(" key2 is " + res[key2])
-      if (res[key2]){
-      tStr += res[key2]
-      tStr += "\r\n"
-      }
-      })
-   console.log("tStr is " + tStr) 
-
-  let blob = new Blob([tStr], { type: 'text/plain;charset=utf-8' })
-  saveAs(blob, 'PlanData.csv')
-  
-   }
 
 }
--- src/app/plans/plans.component.ts
+++ src/app/plans/plans.component.ts
@@ -7,34 +7,39 @@ import { MatTabChangeEvent } from '@angular/material/tabs';
   templateUrl: './plans.component.html',
   styleUrls: ['./plans.component.css']
 })
-export class PlansComponent implements OnInit {
+export class PlansComponent {
   [x: string]: any;
   panelOpenState: boolean;
-  data: any;
+  dataArray: any;
+  tabIndex: number;
+  plansDisplayed: any[];
+  allPlans: any[];
+  count: number;
+  
 
-
-  ngOnInit(): void {
-
-  }
   
   constructor(private http: HttpClient) {
-    console.log(" const ");
+    this. tabIndex = 0;                     // set to show MainPage data
+    this .allPlans = Array();
     this.getData().subscribe(res =>{
       this.setData(res);
     })
    }
-  mainQA(event: MatTabChangeEvent){
-    console.log("hellow %o", event);
-  }
+  setPlansShown(event: MatTabChangeEvent){
+    console.log("hellow %o", event.index);
+    this. tabIndex = event.index;               // set to determine shown plans according to tab selected
 
+  }
   getData(){
     var url = 'https://ion.mgh.harvard.edu/cgi-bin/imrtqa/getForQA.php';
-   // var url = 'https://whiteboard.partners.org/esb/FLwbe/proxy.php?MDKey=test';
     return this .http.get(url)
     }
   setData(res ) {
-      console.log("1113333")
-      this.data = res;
-      console.log(this.data)
+      this.dataArray = res;
+      var todayDate = new Date().toISOString().slice(0, 10);
+      this .plansDisplayed = this.dataArray[todayDate]
+      this .allPlans[0] = this .plansDisplayed;
+      this .allPlans[1] = this .plansDisplayed;
+      console.log(  this .dataArray );
     }
 }
