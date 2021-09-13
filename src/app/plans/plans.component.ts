59bd2a06eda2c7b443439c5f9724fa1163a5d6cc
Franklin Lonberg <flonberg@gmail.com>
2021-09-13 07:18:24 -0400

    just trying

--- src/app/plans/plans.component.ts
+++ src/app/plans/plans.component.ts
@@ -12,10 +12,12 @@ export class PlansComponent {
   //panelOpenState: boolean;
   dataArray: any[];
   filteredPlans: any[];
+  f2Plans: any[]
   tabIndex: number;
   plansDisplayed: any[];
   allPlans: any[];
   count: number;
+  testData: any[];
   
 
   constructor(private http: HttpClient) {
@@ -36,28 +38,66 @@ export class PlansComponent {
     }
   setData(res ) {
       this.dataArray = res;
+      console.log(this.dataArray)
       var todayDate = new Date().toISOString().slice(0, 10);
       this .plansDisplayed = this.dataArray[todayDate]
       var index = 0;
       Object.keys(this.dataArray).forEach(key => {
         this. allPlans[index++] = this.dataArray[key]
       })
+
+      this. filter2data(this .allPlans)
       this. filterData(this .allPlans);
+
+    console.log("f2Plans is %o", this .f2Plans)
   }
   filterData(dArray){
     var index = 0;
+    console.log("50 %0 ", dArray[4][25])
     this .filteredPlans= Array();
     Object.keys(dArray).forEach(key => {                    // step thru the plans for each Date
       this .filteredPlans[key] = Array();              // create it
       Object.keys(dArray).forEach(key2 => {                 // step thru each plan
         if (dArray[key][key2]   ){                          // if there IS data
-          if (+dArray[key][key2]['flowbit'] !== dArray[key][key2]['flowbitAndWorkbit'] ) { // flowbit == flowbitAndWorkbigt -> QAComplete
+
+          if (dArray[key][key2]['UnitNumber'] == '7055544'){
+            console.log("StartDate is "+   dArray[key][key2]['StartDate'] + "  flowbit is "  + dArray[key][key2]['flowbit'] + "flowAndWorkbit" + dArray[key][key2]['flowbitAndWorkbit'] );
+          }
+          if (+dArray[key][key2]['flowbit'] !== dArray[key][key2]['flowbitAndWorkbit'] ) 
+          { // flowbit == flowbitAndWorkbigt -> QAComplete
+   
             this .filteredPlans[key].push(dArray[key][key2]); // push the plan into the array
+            if (dArray[key][key2]['UnitNumber'] == '7055544'){
+              console.log("2222 key is " + key + " StartDate is "+   dArray[key][key2]['planIdx'] + "  flowbit is "  + dArray[key][key2]['flowbit'] + "flowAndWorkbit" + dArray[key][key2]['flowbitAndWorkbit'] );
+              console.log(this .filteredPlans[key]);
+            }
           }
         }
+
       })
     })
-    console.log("filteredPlans is %o", this .filteredPlans)
+
+  }
+  filter2data(dArray){
+    this. f2Plans = Array();
+    for (let entry of dArray) {
+      if (!this. f2Plans[entry[0]['StartDate']])
+      this. f2Plans [entry[0]['StartDate']] = Array();
+        for (let e2 of entry){
+         // if ( entry[0]['StartDate'] == '2021-09-13')
+          {
+            if (+e2['flowbit'] !== e2['flowbitAndWorkbit'] ) {
+           //   console.log("e2 flowbit is " + e2['flowbit'] + "fbat is " +e2['flowbitAndWorkbit'] )
+            this. f2Plans [entry[0]['StartDate']].push(e2)
+            } 
+        }
+      }
+    }
+    console.log("f2Plans 88888 is %o", this .f2Plans['2021-09-13'])
+    for(let i=0; i<this.f2Plans.length; i++){
+      console.log("rrrr");
+  }
+
   }
   linacClass(str){
     if (str.indexOf("TrueBeam") !== -1)
